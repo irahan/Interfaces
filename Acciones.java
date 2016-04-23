69b33112279ba9fefec9ddca0e525281378cb052
irahan <irahan@gmail.com>
2016-04-23 10:12:33 -0500

    subiendo

---
+++ Acciones.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of interface Acciones here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Acciones
+{
+   public void desplazar();
+   public void jugar();
+   public void alimentarse();
+   public void dormir();
+}
